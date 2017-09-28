387a6a0a3a1aaee024c64162a931d43513622bcf
wenjun <wenjun@wenjundeMacBook-Pro.local>
2017-09-28 18:26:00 +0800

    完善

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,15 +1,33 @@
+// ios内
 export function isIOS() {
     return true
 }
 
+// 安卓内
 export function isAndroid() {
     return true
 }
 
+// 微信内
 export function isWeChat() {
     return true
 }
 
+// 叮叮内
+export function isDingDing() {
+    return true
+}
+
+// 混合app
 export function isHybrid(){
+    return false
+}
+
+// 移动网页
+export function isWap(){
+    return false
+}
+// pc网页
+export function isWeb(){
     return false
 }
